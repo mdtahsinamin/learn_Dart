2d023af9e94ab3130df82ca144ef75dea95bafc1
Md. Tahsin Amin <tahsin16@cse.pstu.ac.bd>
2023-04-14 23:26:51 +0600

    first commit

---
+++ dart_assignment.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<String> phoneNumbers = [
+    '+88',
+    '01768131685',
+    '01768171985',
+    '01768111286',
+    '01768131685'
+  ];
+
+  String firstNumber = phoneNumbers[0] + phoneNumbers[1];
+  String secondNumber = phoneNumbers[0] + phoneNumbers[2];
+  String thirdNumber = phoneNumbers[0] + phoneNumbers[3];
+  String fourthNumber = phoneNumbers[0] + phoneNumbers[4];
+  print(firstNumber);
+  print(secondNumber);
+  print(thirdNumber);
+  print(fourthNumber);
+}
